a04c9c543f482596037cbc249673c85266879135
yashjawale <yash25.j@gmail.com>
2026-07-26 19:12:30 +0530

    feat: use patternfly components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import '@patternfly/patternfly/patternfly.css';
+import '@patternfly/patternfly/patternfly-addons.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
